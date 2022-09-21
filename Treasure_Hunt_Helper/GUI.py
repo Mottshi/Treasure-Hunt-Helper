912851d3a17dbc8a5a4187a50d57a228cdb289af
Mottshi <mxttshi@gmail.com>
2022-09-21 20:22:14 +0300

    Updated GUI

--- Treasure_Hunt_Helper/GUI.py
+++ Treasure_Hunt_Helper/GUI.py
@@ -1,36 +1,81 @@
+import algorithm
+import random
 import PySimpleGUI as sg
 
 sg.theme('Black')
 
 
-layout =[[sg.Text('First', size=(20,1), font='Lucida, 20', justification='left')],
-         [sg.Combo(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], size=(20, 30), font='Lucida, 13', key='first')],
-         [sg.Radio('In the row and in the correct spot', "RADIO1", font='Lucida,10' ,default=False)],
-         [sg.Radio('In the row but in the wrong spot', "RADIO1", font='Lucida,10', default=True)],
-         [sg.Radio('Not in the row in any spot', "RADIO1", font='Lucida,10', default=True)],
-         [sg.Text('Second', size=(20, 1), font='Lucida, 20', justification='left')],
-         [sg.Combo(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], size=(20, 30), font='Lucida, 13', key='second')],
-         [sg.Radio('In the row and in the correct spot', "RADIO2", font='Lucida,10', default=False)],
-         [sg.Radio('In the row but in the wrong spot', "RADIO2", font='Lucida,10', default=True)],
-         [sg.Radio('Not in the row in any spot', "RADIO2", font='Lucida,10', default=True)],
-         [sg.Text('Third', size=(20, 1), font='Lucida, 20',  justification='left')],
-         [sg.Combo(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], size=(20, 30), font='Lucida, 13', key='third')],
-         [sg.Radio('In the row and in the correct spot', "RADIO3", font='Lucida,10', default=False)],
-         [sg.Radio('In the row but in the wrong spot', "RADIO3", font='Lucida,10', default=True)],
-         [sg.Radio('Not in the row in any spot', "RADIO3", font='Lucida,10',  default=True)],
-         [sg.Text('Fourth', size=(20, 1), font='Lucida, 20', justification='left')],
-         [sg.Combo(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], size=(20, 30), font='Lucida, 13', key='fourth')],
-         [sg.Radio('In the row and in the correct spot', "RADIO4", font='Lucida,10', default=False)],
-         [sg.Radio('In the row but in the wrong spot', "RADIO4", font='Lucida,10', default=True)],
-         [sg.Radio('Not in the row in any spot', "RADIO4", font='Lucida,10', default=True)],
-         [sg.Text('Fifth', size=(20, 1), font='Lucida, 20', justification='left')],
-         [sg.Combo(['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'], size=(20, 30), font='Lucida, 13', key='fifth')],
-         [sg.Radio('In the row and in the correct spot', "RADIO5", font='Lucida,10', default=False)],
-         [sg.Radio('In the row but in the wrong spot', "RADIO5", font='Lucida,10', default=True)],
-         [sg.Radio('Not in the row in any spot', "RADIO5", font='Lucida,10', default=True)],
-         [sg.T("")],
-         [sg.Button('Okay', size=(10,1), font='Lucida, 20')]]
+layout = [
+    [sg.Text('Number of resources', font='Courier` 20'),
+     sg.InputText(key='-INPUT-', font='Courier` 20'),
+     sg.Button('Confirm', key='-CONFIRM-', font='Courier` 20')
+     ],
+    [sg.Text()],
+    [sg.Text('0 - Correct!, 1 - Needed for another pirate, 2 - Not needed anywhere, 3 - Update the combination', font='Courier` 20')],
+    [sg.Text()],
+    [sg.Text('Response', font='Courier` 20'),
+     sg.InputText(key='-INPUT1-', font='Courier` 20'),
+     sg.Button('Confirm', key='-CONFIRM1-', font='Courier` 20')
+     ],
+    [sg.Text('Output', key='-OUTPUT-', font='Courier` 20')],
+    [sg.CloseButton('Close', font='Courier` 20')]
+]
 
-win = sg.Window('Helper', layout)
-e, v = win.read()
-win.close()
+window = sg.Window('Treasure Hunt Helper', layout)
+while True:
+    event, values = window.read()
+
+
+    if event == '-CONFIRM-':
+        match int(values['-INPUT-']):
+            case 2:
+                elements = ['A', 'B']
+                combination = ['A', 'B', 'A', 'B', 'A']
+            case 3:
+                elements = ['A', 'B', 'C']
+                combination = ['A', 'B', 'C', 'A', 'B']
+            case 4:
+                elements = ['A', 'B', 'C', 'D']
+                combination = ['A', 'B', 'C', 'D', 'A']
+            case 5:
+                elements = ['A', 'B', 'C', 'D', 'E']
+                combination = ['A', 'B', 'C', 'D', 'E']
+            case 6:
+                elements = ['A', 'B', 'C', 'D', 'E', 'F']
+                combination = ['A', 'B', 'C', 'D', 'E']
+            case 7:
+                elements = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+                combination = ['A', 'B', 'C', 'D', 'E']
+            case 8:
+                elements = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+                combination = ['A', 'B', 'C', 'D', 'E']
+            case 9:
+                elements = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+                combination = ['A', 'B', 'C', 'D', 'E']
+        allpossiblecomb = algorithm.CreateNewComb(elements)
+        window['-OUTPUT-'].update(combination)
+
+    if event == sg.WIN_CLOSED:
+        break
+
+
+    if event == '-CONFIRM1-':
+        response = list(values['-INPUT1-'])
+        window['-INPUT1-']('')
+        if response == ['3']:
+            combination = random.choice(allpossiblecomb)
+        for i in range(len(response)):
+            if response[i] == '0':
+                allpossiblecomb = algorithm.SortingForZero(allpossiblecomb, combination, i)
+        for i in range(len(response)):
+            if response[i] == '1':
+                allpossiblecomb = algorithm.SortingForOne(allpossiblecomb, combination, i)
+        for i in range(len(response)):
+            if response[i] == '2':
+                allpossiblecomb = algorithm.SortingForTwo(allpossiblecomb, combination, i)
+        combination = random.choice(allpossiblecomb)
+        window['-OUTPUT-'].update(combination)
+        if response == ['0','0', '0', '0', '0']:
+            window['-OUTPUT-'].update('')
+
+window.close()
